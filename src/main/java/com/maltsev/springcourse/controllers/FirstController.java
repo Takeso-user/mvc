3e0ed2e4e26a605bac4620a5576f8d193d435528
me <me>
2020-12-27 15:46:16 +0100

    lesson19

--- src/main/java/com/maltsev/springcourse/controllers/FirstController.java
+++ src/main/java/com/maltsev/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.maltsev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +14,11 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname",required = false) String surname) {
-        System.out.println("hello! " + name + " " + surname);
+                            @RequestParam(value = "surname",required = false) String surname,
+                            Model model) {
+//        System.out.println("hello! " + name + " " + surname);
+       model.addAttribute("message","hello "+name+"  "+surname);
+
         return "first/hello";
     }
 
